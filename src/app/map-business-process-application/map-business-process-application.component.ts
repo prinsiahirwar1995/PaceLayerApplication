487e5761e825064c84aa7b10eea6dd21ce404d53
Prinsi Ahirwar <PrinsiA@quinnox.com>
2020-03-23 18:41:37 +0530

    caption table changes

--- src/app/map-business-process-application/map-business-process-application.component.ts
+++ src/app/map-business-process-application/map-business-process-application.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Injectable, ViewChild, ElementRef } from '@angular/c
 
 import { DropdownModule } from 'primeng/dropdown';
 
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder,Validators, FormControl } from '@angular/forms';
 
 import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 
@@ -26,7 +26,8 @@ import { viewClassName } from '@angular/compiler';
 export class MapBusinessProcessApplicationComponent implements OnInit {
 
   title = 'PaceLayerUI';
-  
+  form: FormGroup;
+  submitted = false;
   // oppoSuitsForm: FormGroup
 
   //submitted = false;private formBuilder: FormBuilder,
@@ -56,6 +57,7 @@ export class MapBusinessProcessApplicationComponent implements OnInit {
   @ViewChild('tableforddl') tableforddl: ElementRef;
   @ViewChild('tablewithoutddl') tablewithoutddl: ElementRef;
   @ViewChild('lblMessage') lblMessage: ElementRef;
+
   Onchangedropdown(val) {
     
     
@@ -83,21 +85,28 @@ export class MapBusinessProcessApplicationComponent implements OnInit {
 
   ngOnInit() {
 
-    
+    this.form = this.fb.group({
+      PortfolioID: new FormControl('',Validators.required) ,
+      ApplicationID: new FormControl('',Validators.required),
+      BPID: new FormControl('',Validators.required),
+      SupportLevelID: new FormControl('',Validators.required)
+    })
     
     this.httpService.get(this.sPath + "portfolios").subscribe(portfoliodata => {
 
       this.httpService.get(this.sPath + "BProcess").subscribe(bpdata => {
 
         this.httpService.get(this.sPath + "GetSupports").subscribe(supportlevel => {
+
           this.httpService.get(this.sPath + "getApplBProcess/all/0").subscribe(tabledata => {
 
             this.mapData = portfoliodata[0];//fill data from api to mapdata
-
+            
             this.BPData = bpdata[0];//fill data from api to mapdata
-
+            
+            
             this.SupportLevel = supportlevel[0];//fill data from api to mapdata
-
+          
             this.Tabledata = tabledata[1];
              
           });
@@ -131,10 +140,16 @@ export class MapBusinessProcessApplicationComponent implements OnInit {
 
   })
 
-
+// convenience getter for easy access to form fields
+get f() { return this.form.controls; }
 
   onSubmit() {
-
+    
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+  }
     let headers = new HttpHeaders();
     headers = new HttpHeaders(
       {
@@ -144,23 +159,24 @@ export class MapBusinessProcessApplicationComponent implements OnInit {
       });
     var data = {
 
-      "PortfolioID": this.mapbusForm.value.PortfolioID, "ApplicationID": this.mapbusForm.value.ApplicationID
+      "PortfolioID": this.form.value.PortfolioID, "ApplicationID": this.form.value.ApplicationID
 
-      , "BprocessID": this.mapbusForm.value.BPID, "SupportOptionID": this.mapbusForm.value.SupportLevelID
+      , "BprocessID": this.form.value.BPID, "SupportOptionID": this.form.value.SupportLevelID
     };
 
-
+console.log(data)
 
     return this.httpService.post("http://pacelayerapi.azurewebsites.net/masterApi/addport/", data, { headers: headers }).subscribe(data => {
 
 
       this.posts = data;
       alert('Record added successfully.');
+      console.log(this.posts)
       this.Onchangedropdown(this.mapbusForm.value.PortfolioID);
       // show data in console
     }, (error: Response) => {
       if (error.status === 424)
-        alert('This Post is Already Added');
+        alert('This Process is Already Added!!!');
       else
         alert('An Unhandled Error');
       console.log(error);
